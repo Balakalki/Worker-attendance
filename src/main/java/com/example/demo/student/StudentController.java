f3667396c85891cef1947c16b86b725613e6750d
balakalki.a <balakalki.a@thekaios.com>
2026-05-25 23:44:10 +0530

    feat: folder structure and supabase connection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,27 +5,32 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
 @AllArgsConstructor
 public class StudentController {
 
-    private final StudentService studentService;
+    // private final StudentService studentService;
 
     @GetMapping
-    public List<Student> getAllStudents() {
-        return studentService.getAllStudents();
+    public List<String> getAllStudents() {
+        return new ArrayList<>(){
+            {
+                add("John Doe");
+            }
+        };
     }
 
-    @PostMapping
-    public void addStudent(@Valid @RequestBody Student student) {
-        studentService.addStudent(student);
-    }
+    // @PostMapping
+    // public void addStudent(@Valid @RequestBody Student student) {
+    //     studentService.addStudent(student);
+    // }
 
-    @DeleteMapping(path = "{studentId}")
-    public void deleteStudent(
-            @PathVariable("studentId") Long studentId) {
-        studentService.deleteStudent(studentId);
-    }
+    // @DeleteMapping(path = "{studentId}")
+    // public void deleteStudent(
+    //         @PathVariable("studentId") Long studentId) {
+    //     studentService.deleteStudent(studentId);
+    // }
 }
---
+++ src/main/java/com/example/demo/workforce/SettlementStatus.java
@@ -0,0 +1,5 @@
+package com.example.demo.workforce;
+
+public class SettlementStatus {
+    
+}
